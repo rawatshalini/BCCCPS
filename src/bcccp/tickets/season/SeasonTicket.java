f1d4c58618c248851427bdc3e927626ae23095fe
rponna <rponna@users.noreply.github.com>
2017-08-23 22:26:42 +1000

    Implemented getter method for endValidPeriod variable in SeasonTicket class

--- src/bcccp/tickets/season/SeasonTicket.java
+++ src/bcccp/tickets/season/SeasonTicket.java
@@ -41,8 +41,7 @@ public class SeasonTicket implements ISeasonTicket {
 
 	@Override
 	public long getEndValidPeriod() {
-		// TODO Auto-generated method stub
-		return 0;
+		return endValidPeriod;
 	}
 
 	@Override
