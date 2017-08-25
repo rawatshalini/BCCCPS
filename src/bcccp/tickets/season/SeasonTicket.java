b7ccfa4f46db39e7ec129fd411970fb9adf95f36
rawatshalini <30823647+rawatshalini@users.noreply.github.com>
2017-08-25 00:24:45 -0700

    Revert "Revert "line 30 changed""
    This reverts commit 0e2e2f6a58fd25c0de753613864f19aabeae2b24.

--- src/bcccp/tickets/season/SeasonTicket.java
+++ src/bcccp/tickets/season/SeasonTicket.java
@@ -27,7 +27,7 @@ public class SeasonTicket implements ISeasonTicket {
 
 	@Override
 	public String getId() {
-		// TODO Auto-generated method stub
+		//this method takes the id of season customer 
 		return null;
 	}
 
