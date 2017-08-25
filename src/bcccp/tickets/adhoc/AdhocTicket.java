5561dcf85febfdaf05296e86d0084b462b0a42c0
rawatshalini <30823647+rawatshalini@users.noreply.github.com>
2017-08-25 16:19:42 -0700

    AdhocTiket constructor
    AdhocTiket constructor modified, line no 18 to 20 changed

--- src/bcccp/tickets/adhoc/AdhocTicket.java
+++ src/bcccp/tickets/adhoc/AdhocTicket.java
@@ -15,8 +15,10 @@ public class AdhocTicket implements IAdhocTicket {
 	
 	
 	public AdhocTicket(String carparkId, int ticketNo, String barcode) {
-		//TDO Implement constructor
-	}
+		this.carparkId = carparkId;
+		this.ticketNo = ticketNo;
+		this.barcode = barcode;
+}
 
 
 	@Override
