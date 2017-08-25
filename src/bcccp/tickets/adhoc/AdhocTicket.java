6b4ca05e5c335b15bdc8e76235cda094a180359c
rawatshalini <30823647+rawatshalini@users.noreply.github.com>
2017-08-25 16:52:33 -0700

    public void pay modified
    line 63 and 64 changed

--- src/bcccp/tickets/adhoc/AdhocTicket.java
+++ src/bcccp/tickets/adhoc/AdhocTicket.java
@@ -60,8 +60,9 @@ public class AdhocTicket implements IAdhocTicket {
 
 	@Override
 	public void pay(long dateTime, float charge) {
-		// TODO Auto-generated method stub
-		
+		this.paidDateTime = dateTime;
+		this.charge = charge;
+
 	}
 
 
